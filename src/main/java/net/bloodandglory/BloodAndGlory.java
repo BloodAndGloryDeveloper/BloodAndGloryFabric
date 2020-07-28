0f864271a0208b9644e54be2dc55c0ccdaf82146
yaesey <18177114151@163.com>
2020-07-28 20:53:25 +0800

    初始化

---
+++ src/main/java/net/bloodandglory/BloodAndGlory.java
@@ -0,0 +1,12 @@
+package net.bloodandglory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BloodAndGlory implements ModInitializer {
+    public static final String  MOD_ID = "bloodandglory";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
