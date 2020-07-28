d525e18724c3b2c315a925d17265438357742b9e
yaesey <18177114151@163.com>
2020-07-28 22:18:34 +0800

    从1.12旧版本中重新引入材质

---
+++ src/main/java/net/bloodandglory/item/ItemBAG.java
@@ -0,0 +1,9 @@
+package net.bloodandglory.item;
+
+import net.minecraft.item.Item;
+
+public class ItemBAG extends Item {
+    public ItemBAG(Settings settings) {
+        super(settings);
+    }
+}
